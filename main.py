e643c9a327d665b03ca497588a842dec74a4ffdd
nicojsuarez <67976147+nicojsuarez@users.noreply.github.com>
2023-10-30 07:18:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from fastapi.responses import RedirectResponse # esta libreria funciona para la 
 app=FastAPI()
 
 # Cargar los datos desde los archivos CSV y Parquet
-genre_data = pd.read_csv(r'C:\Users\Administrador\Desktop\vitual\MlOprsSteam\genre.csv')
-items_data = pd.read_parquet(r'C:\Users\Administrador\Desktop\vitual\MlOprsSteam\items1.parquet')
-reviews_data = pd.read_csv(r'C:\Users\Administrador\Desktop\vitual\MlOprsSteam\reviews.csv')
-steam_data = pd.read_csv(r'C:\Users\Administrador\Desktop\vitual\MlOprsSteam\steam.csv')
+genre_data = pd.read_csv('genre.csv')
+items_data = pd.read_parquet('items1.parquet')
+reviews_data = pd.read_csv('reviews.csv')
+steam_data = pd.read_csv('steam.csv')
 
 
 @app.get("/", include_in_schema=False)  # esta funcion genera que la url ya venga con /doc*/ haciendo que se abra directo el API. 
